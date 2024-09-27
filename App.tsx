ca917249461df9f2987bb516bb7f6723395f2499
fellington <44418534+fellington@users.noreply.github.com>
2024-09-27 15:43:03 -0700

    iOS Error Fix

--- App.tsx
+++ App.tsx
@@ -1,13 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './app/screens/Login';
 import HomePage from './app/screens/HomePage';
 import InstrImages from './app/screens/InstrImages';
 import Support from './app/screens/Support';
-import { useEffect, useState } from 'react';
-import { onAuthStateChanged, User } from 'firebase/auth';
-import { FIREBASE_AUTH } from './FirebaseConfig';
-
 
 const Stack = createNativeStackNavigator();
 
@@ -24,22 +19,10 @@ function InsideLayout() {
 }
 
 export default function App() {
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      console.log('user', user);
-      setUser(user);
-    });
-  }, []);
   return (
-    <NavigationContainer> 
-      <Stack.Navigator initialRouteName = 'Login'>
-        {user ? (
-          <Stack.Screen name='Inside' component={InsideLayout} options ={{headerShown: false}}/>
-        ) : (
-          <Stack.Screen name='Login' component={Login} options ={{headerShown: false}}/>
-        )}    
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inside">
+        <Stack.Screen name="Inside" component={InsideLayout} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
